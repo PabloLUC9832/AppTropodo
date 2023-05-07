0f1ea29a7a9f821ca7e51c7b3ea8b6d498300a96
PabloLUC9832 <lucaspablo340@outlook.com>
2023-05-06 23:25:15 -0600

    crud

--- resources/views/artropodo/create.blade.php
+++ resources/views/artropodo/create.blade.php
@@ -47,18 +47,7 @@
                 <x-input name="habitat" message="Hábitat"></x-input>
 
                 <x-input name="distribucionGeografica" message="Distribución geográfica"></x-input>
-                {{--
-                <button id="btn-add" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    <span class="sr-only">Icon description</span>
-                </button>
 
-                <div class="li-container">
-                    <ul>
-
-                    </ul>
-                </div>
-                --}}
                 <x-input name="color" message="Color"></x-input>
 
                 <x-input name="peligroso" message="Peligroso"></x-input>
@@ -87,7 +76,7 @@
 
                 </div>
 
-                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancelar</button>
+                <a href="{{route('artropodo.index')}}" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancelar</a>
 
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Registrar</button>
 
--- resources/views/artropodo/index.blade.php
+++ resources/views/artropodo/index.blade.php
@@ -15,7 +15,10 @@
 
 </head>
 <body class="fondo">
+
+
 @include('layouts.navigation')
+
 <div class="fondo m-10">
 
     <div>
@@ -23,6 +26,10 @@
             <div class="md:col-span-1">
                 <div class="px-4 sm:px-0">
                     <h3 class="text-lg font-medium leading-6 text-gray-900">Lista de artrópodos</h3>
+                    <a href="{{ route('artropodo.create') }}" class="mt-2 inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                        Agregar artrópodo
+                        <svg width="15px" height="15px" viewBox="0 0 1024 1024" class="icon ml-2"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M512 1024C229.7 1024 0 794.3 0 512S229.7 0 512 0s512 229.7 512 512-229.7 512-512 512z m0-938.7C276.7 85.3 85.3 276.7 85.3 512S276.7 938.7 512 938.7 938.7 747.3 938.7 512 747.3 85.3 512 85.3z" fill="#3688FF" /><path d="M682.7 554.7H341.3c-23.6 0-42.7-19.1-42.7-42.7s19.1-42.7 42.7-42.7h341.3c23.6 0 42.7 19.1 42.7 42.7s-19.1 42.7-42.6 42.7z" fill="#5F6379" /><path d="M512 725.3c-23.6 0-42.7-19.1-42.7-42.7V341.3c0-23.6 19.1-42.7 42.7-42.7s42.7 19.1 42.7 42.7v341.3c0 23.6-19.1 42.7-42.7 42.7z" fill="#5F6379" /></svg>
+                    </a>
                 </div>
             </div>
 
@@ -38,42 +45,16 @@
     <div class="md:grid md:grid-cols-4 md:gap-6">
 
         @if(count($artropodos) <= 0)
-            No se han encontrado artropodos
 
+            <h4>No se han encontrado artrópodos.
+                <a href="{{ route('artropodo.create') }}" class="mt-2 inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    Agregar artrópodo
+                    <svg width="15px" height="15px" viewBox="0 0 1024 1024" class="icon ml-2"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M512 1024C229.7 1024 0 794.3 0 512S229.7 0 512 0s512 229.7 512 512-229.7 512-512 512z m0-938.7C276.7 85.3 85.3 276.7 85.3 512S276.7 938.7 512 938.7 938.7 747.3 938.7 512 747.3 85.3 512 85.3z" fill="#3688FF" /><path d="M682.7 554.7H341.3c-23.6 0-42.7-19.1-42.7-42.7s19.1-42.7 42.7-42.7h341.3c23.6 0 42.7 19.1 42.7 42.7s-19.1 42.7-42.6 42.7z" fill="#5F6379" /><path d="M512 725.3c-23.6 0-42.7-19.1-42.7-42.7V341.3c0-23.6 19.1-42.7 42.7-42.7s42.7 19.1 42.7 42.7v341.3c0 23.6-19.1 42.7-42.7 42.7z" fill="#5F6379" /></svg>
+                </a>
+            </h4>
         @else
 
             @foreach($artropodos as $artropodo)
-                {{--
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="https://desarrollointeracciones.blob.core.windows.net/files/{{$artropodo->foto}}" alt="{{ $artropodo->nombre_comun }}" />
-                    </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $artropodo->nombre_comun }}</h4>
-                            <h2 class="mb-2 text-2xl font-light tracking-tight text-gray-900 dark:text-white">{{ $artropodo->nombre_cientifico }}</h2>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> <span class="font-bold"> Clasificación:</span> {{ $artropodo->clasificacion }}</p>
-
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> <span class="font-bold"> Hábitat:</span> {{ $artropodo->habitat }}</p>
-
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> <span class="font-bold"> Distribución geográfica:</span> {{ $artropodo->distribucion_geografica }}</p>
-
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> <span class="font-bold"> Color:</span> {{ $artropodo->color }}</p>
-
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> <span class="font-bold"> Peligroso:</span> {{ $artropodo->peligroso }}</p>
-
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> <span class="font-bold"> Número de patas:</span> {{ $artropodo->patas }}</p>
-
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Editar
-                        </a>
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Eliminar
-                        </a>
-                    </div>
-                </div>
-            --}}
 
                 <div class="max-w-sm rounded overflow-hidden shadow-lg">
                     <img class="w-full" src="https://desarrollointeracciones.blob.core.windows.net/files/{{$artropodo->foto}}" alt="{{ $artropodo->nombre_comun }}">
@@ -115,10 +96,6 @@
 
 
     </div>
-
-
-
-
 
 </div>
 
