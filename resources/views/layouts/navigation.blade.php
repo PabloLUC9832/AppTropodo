782292e00731d6c13df06aa17290d6b40e780f7c
PabloLUC9832 <lucaspablo340@outlook.com>
2023-05-07 20:52:33 -0600

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,6 +76,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Inicio') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('artropodo.index')" :active="request()->routeIs('artropodo.index')">
+                {{ __('Artrópodos') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('artropodo.clasificaciones')" :active="request()->routeIs('artropodo.clasificaciones')">
+                {{ __('Clasificaciones') }}
+            </x-responsive-nav-link>            
         </div>
 
         <!-- Responsive Settings Options -->
